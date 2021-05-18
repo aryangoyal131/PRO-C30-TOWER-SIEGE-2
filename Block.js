3320aaaa0d6aa1f817b8a88616ab651bc004f3b2
aryangoyal131 <76836401+aryangoyal131@users.noreply.github.com>
2021-05-18 20:18:10 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,34 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.image = loadImage("block.png");
+      World.add(world, this.body);
+    }
+    display(){
+
+      var pos= this.body.position;
+      
+      if(this.body.speed < 3){
+        
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.width, this.height);
+        this.Visiblity = this.Visiblity - 5;
+        tint(255,this.Visiblity);
+        pop();
+
+      }
+      else{
+        World.remove(world, this.body);
+      }
+
+
+    }
+}
