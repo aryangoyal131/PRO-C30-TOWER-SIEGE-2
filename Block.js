0c331fdd567cd6cfa3bbbfa0c87e93cc8eb33a7f
aryangoyal131 <76836401+aryangoyal131@users.noreply.github.com>
2021-05-20 13:51:36 +0530

    Add files via upload

--- Block.js
+++ Block.js
@@ -7,6 +7,7 @@ class Block{
       this.body = Bodies.rectangle(x, y, width, height, options);
       this.width = width;
       this.height = height;
+      this.Visiblity = 255;
       this.image = loadImage("block.png");
       World.add(world, this.body);
     }
@@ -20,7 +21,7 @@ class Block{
         translate(this.body.position.x, this.body.position.y);
         imageMode(CENTER);
         image(this.image, 0, 0, this.width, this.height);
-        this.Visiblity = this.Visiblity - 5;
+        this.Visiblity = this.Visiblity - 3;
         tint(255,this.Visiblity);
         pop();
 
